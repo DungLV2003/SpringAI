65f097109c4b82fcb1111823c2ef1f9df1bd81ec
Le Van Dung <ledung20102003@gmail.com>
2025-07-20 09:40:46 +0700

    Init chat memory and postgresSql

--- src/main/java/com/dungle/spring_ai_demo/controller/ChatController.java
+++ src/main/java/com/dungle/spring_ai_demo/controller/ChatController.java
@@ -5,6 +5,7 @@ import com.dungle.spring_ai_demo.dto.ChatRequest;
 import com.dungle.spring_ai_demo.dto.ExpenseInfo;
 import com.dungle.spring_ai_demo.dto.FilmInfo;
 import com.dungle.spring_ai_demo.service.ChatService;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,24 +17,28 @@ import java.util.List;
 @RestController
 public class ChatController {
     private final ChatService chatService;
+    private final JdbcChatMemoryRepository chatMemoryRepository;
 
-    public ChatController(ChatService chatService) {
+    public ChatController(ChatService chatService,
+                          JdbcChatMemoryRepository chatMemoryRepository) {
         this.chatService = chatService;
+        this.chatMemoryRepository = chatMemoryRepository;
     }
 
-//    @PostMapping("/chat")
-//    String chat(@RequestBody ChatRequest request){
-//        return chatService.chat(request);
-//    }
+    @PostMapping("/chat")
+    String chat(@RequestBody ChatRequest request){
+        return chatService.chat(request);
+    }
+
 //    @PostMapping("/chat")
 //    List<FilmInfo> chat(@RequestBody ChatRequest request){
 //        return chatService.chat(request);
 //    }
 
-        @PostMapping("/chat")
-        ExpenseInfo chat(@RequestBody ChatRequest request){
-            return chatService.chat(request);
-         }
+       @PostMapping("/chatForExpenseInfo")
+       ExpenseInfo chatExpenseInfo(@RequestBody ChatRequest request){
+           return chatService.chatForExpenseInfo(request);
+        }
 
 
 //    @PostMapping("/chat-with-image")
--- src/main/java/com/dungle/spring_ai_demo/service/ChatService.java
+++ src/main/java/com/dungle/spring_ai_demo/service/ChatService.java
@@ -5,6 +5,10 @@ import com.dungle.spring_ai_demo.dto.ChatRequest;
 import com.dungle.spring_ai_demo.dto.ExpenseInfo;
 import com.dungle.spring_ai_demo.dto.FilmInfo;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.ChatOptions;
@@ -20,36 +24,24 @@ import java.util.List;
 @Service
 public class ChatService {
     private final ChatClient chatClient;
+    private final JdbcChatMemoryRepository jdbcChatMemoryRepository;
 
-    public ChatService(ChatClient.Builder builder) {
-        chatClient = builder.build();
+    public ChatService(ChatClient.Builder builder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+        this.jdbcChatMemoryRepository = jdbcChatMemoryRepository;
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(30)
+                .build();
+
+        chatClient = builder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
-//    public String chat(ChatRequest request) {
-//        SystemMessage systemMessage = new SystemMessage("""
-//                You are DungLe.AI
-//                 You should response with a super funny voice""");
-//        UserMessage userMessage = new UserMessage(request.message());
-//        Prompt prompt = new Prompt(systemMessage, userMessage);
-//
-//        return chatClient.prompt(prompt)
-//               .call()
-//               .content();
-//    }
 
-//    public List<FilmInfo> chat(ChatRequest request) {
-//        SystemMessage systemMessage = new SystemMessage("""
-//                You are DungLe.AI
-//                 You should response with a super funny voice""");
-//        UserMessage userMessage = new UserMessage(request.message());
-//        Prompt prompt = new Prompt(systemMessage, userMessage);
-//
-//        return chatClient.prompt(prompt)
-//                .call()
-//                .entity(new ParameterizedTypeReference<List<FilmInfo>>() {
-//                });
-//    }
 
-    public ExpenseInfo chat(ChatRequest request) {
+    public String chat(ChatRequest request) {
+        String converstationId = "conversation2";
         SystemMessage systemMessage = new SystemMessage("""
                 You are DungLe.AI
                  You should response with a super funny voice""");
@@ -57,6 +49,38 @@ public class ChatService {
         Prompt prompt = new Prompt(systemMessage, userMessage);
 
         return chatClient.prompt(prompt)
+                .advisors(advisor -> advisor.param(
+                        ChatMemory.CONVERSATION_ID, converstationId
+                ))
+               .call()
+               .content();
+    }
+
+    public List<FilmInfo> chatForFileInfo(ChatRequest request) {
+        SystemMessage systemMessage = new SystemMessage("""
+                You are DungLe.AI
+                 You should response with a super funny voice""");
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return chatClient.prompt(prompt)
+                .call()
+                .entity(new ParameterizedTypeReference<List<FilmInfo>>() {
+                });
+    }
+
+    public ExpenseInfo chatForExpenseInfo(ChatRequest request) {
+        String converstationId = "conversation1";
+        SystemMessage systemMessage = new SystemMessage("""
+                You are DungLe.AI
+                You should response with a super funny voice""");
+        UserMessage userMessage = new UserMessage(request.message());
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+
+        return chatClient.prompt(prompt)
+                .advisors(advisor -> advisor.param(
+                        ChatMemory.CONVERSATION_ID, converstationId
+                ))
                 .call()
                 .entity(ExpenseInfo.class);
     }
